c4ea9947ad96a7ae3bcdb7f6a09c98bda8e113c0
ganeshlakhe1220 <39594910-ganeshlakhe1220@users.noreply.replit.com>
2025-02-14 11:06:55 +0000

    Agent query: I've restored the registration page to its original version. Can you check if the registration form is now working correctly?
    Revert changes to registration page styling; restore original design.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/dce5a37d-1c5c-4535-9875-910f965268ef/ebda046d-5fca-48f7-b6da-8dc9fc002bf7.jpg

--- client/src/pages/auth/register.tsx
+++ client/src/pages/auth/register.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import { Navigate, Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '@/contexts/AuthContext';
-import { doCreateUserWithEmailAndPassword } from '@/firebase/auth';
-import { Card, CardContent } from "@/components/ui/card";
+import { useAuth } from '../../contexts/AuthContext';
+import { doCreateUserWithEmailAndPassword } from '../../firebase/auth';
 
 export default function Register() {
     const navigate = useNavigate();
@@ -76,83 +75,81 @@ export default function Register() {
 
     return (
         <main className="w-full h-screen flex self-center place-content-center place-items-center bg-[#18212C]">
-            <Card className="w-96 text-gray-600 space-y-5 p-4 shadow-xl border rounded-xl bg-white">
-                <CardContent>
-                    <div className="text-center mb-6">
-                        <div className="mt-2">
-                            <h3 className="text-gray-800 text-xl font-semibold sm:text-2xl">Create a New Account</h3>
-                        </div>
+            <div className="w-96 text-gray-600 space-y-5 p-4 shadow-xl border rounded-xl bg-white">
+                <div className="text-center mb-6">
+                    <div className="mt-2">
+                        <h3 className="text-gray-800 text-xl font-semibold sm:text-2xl">Create a New Account</h3>
                     </div>
-                    <form onSubmit={onSubmit} className="space-y-4">
-                        <div>
-                            <label className="text-sm text-gray-600 font-bold">Email</label>
-                            <input
-                                type="email"
-                                autoComplete='email'
-                                required
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
-                            />
-                        </div>
-                        <div>
-                            <label className="text-sm text-gray-600 font-bold">Password</label>
-                            <input
-                                disabled={isRegistering}
-                                type="password"
-                                autoComplete='new-password'
-                                required
-                                value={password}
-                                onFocus={() => setShowValidations(true)}
-                                onBlur={() => setShowValidations(false)}
-                                onChange={onPasswordChange}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
-                            />
-                            {showValidations && (
-                                <div className="mt-1 text-sm text-gray-600 space-y-1">
-                                    {passwordValidations.map((validation, index) => (
-                                        <p
-                                            key={index}
-                                            className={`transition-opacity duration-500 flex items-center gap-2 ${validation.valid ? 'text-green-600 fade-out' : 'text-gray-500'}`}
-                                        >
-                                            {validation.valid ? '✔' : '✖'} {validation.text}
-                                        </p>
-                                    ))}
-                                </div>
-                            )}
-                        </div>
-                        <div>
-                            <label className="text-sm text-gray-600 font-bold">Confirm Password</label>
-                            <input
-                                disabled={isRegistering}
-                                type="password"
-                                autoComplete='off'
-                                required
-                                value={confirmPassword}
-                                onChange={onConfirmPasswordChange}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
-                            />
-                            {passwordMatchError && (
-                                <p className="text-red-600 text-sm font-bold mt-1">{passwordMatchError}</p>
-                            )}
-                        </div>
-                        {errorMessage && (
-                            <span className='text-red-600 font-bold'>{errorMessage}</span>
-                        )}
-                        <button
-                            type="submit"
+                </div>
+                <form onSubmit={onSubmit} className="space-y-4">
+                    <div>
+                        <label className="text-sm text-gray-600 font-bold">Email</label>
+                        <input
+                            type="email"
+                            autoComplete='email'
+                            required
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
+                        />
+                    </div>
+                    <div>
+                        <label className="text-sm text-gray-600 font-bold">Password</label>
+                        <input
                             disabled={isRegistering}
-                            className={`w-full px-4 py-2 text-white font-medium rounded-lg ${isRegistering ? 'bg-gray-300 cursor-not-allowed' : 'bg-indigo-600 hover:bg-indigo-700 hover:shadow-xl transition duration-300'}`}
-                        >
-                            {isRegistering ? 'Signing Up...' : 'Sign Up'}
-                        </button>
-                        <div className="text-sm text-center">
-                            Already have an account? {'   '}
-                            <Link to={'/login'} className="text-center text-sm hover:underline font-bold">Continue</Link>
-                        </div>
-                    </form>
-                </CardContent>
-            </Card>
+                            type="password"
+                            autoComplete='new-password'
+                            required
+                            value={password}
+                            onFocus={() => setShowValidations(true)}
+                            onBlur={() => setShowValidations(false)}
+                            onChange={onPasswordChange}
+                            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
+                        />
+                        {showValidations && (
+                            <div className="mt-1 text-sm text-gray-600 space-y-1">
+                                {passwordValidations.map((validation, index) => (
+                                    <p
+                                        key={index}
+                                        className={`transition-opacity duration-500 flex items-center gap-2 ${validation.valid ? 'text-green-600 fade-out' : 'text-gray-500'}`}
+                                    >
+                                        {validation.valid ? '✔' : '✖'} {validation.text}
+                                    </p>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                    <div>
+                        <label className="text-sm text-gray-600 font-bold">Confirm Password</label>
+                        <input
+                            disabled={isRegistering}
+                            type="password"
+                            autoComplete='off'
+                            required
+                            value={confirmPassword}
+                            onChange={onConfirmPasswordChange}
+                            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
+                        />
+                        {passwordMatchError && (
+                            <p className="text-red-600 text-sm font-bold mt-1">{passwordMatchError}</p>
+                        )}
+                    </div>
+                    {errorMessage && (
+                        <span className='text-red-600 font-bold'>{errorMessage}</span>
+                    )}
+                    <button
+                        type="submit"
+                        disabled={isRegistering}
+                        className={`w-full px-4 py-2 text-white font-medium rounded-lg ${isRegistering ? 'bg-gray-300 cursor-not-allowed' : 'bg-indigo-600 hover:bg-indigo-700 hover:shadow-xl transition duration-300'}`}
+                    >
+                        {isRegistering ? 'Signing Up...' : 'Sign Up'}
+                    </button>
+                    <div className="text-sm text-center">
+                        Already have an account? {'   '}
+                        <Link to={'/login'} className="text-center text-sm hover:underline font-bold">Continue</Link>
+                    </div>
+                </form>
+            </div>
         </main>
     );
 }
