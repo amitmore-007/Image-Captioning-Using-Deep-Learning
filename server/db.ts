f8587df0189998d06dc3ac6a88fc19611eb9d7c8
Ganeshrlakhe <140993874+Ganeshrlakhe@users.noreply.github.com>
2025-02-19 16:29:16 +0000

    Restored to '49a0d0a4f4d18cd512a0ab5bbfc1b2790fd60a37'
    Replit-Restored-To:49a0d0a4f4d18cd512a0ab5bbfc1b2790fd60a37

--- server/db.ts
+++ server/db.ts
@@ -13,9 +13,7 @@ if (!process.env.DATABASE_URL) {
 
 const pool = new Pool({ 
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: true
 });
 
 // Test the connection
